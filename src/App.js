52d0b84eb45bdf24c526b299d763189cfb0e379c
Nikhil Malagi <nikhil.malagi@daimler.com>
2021-02-18 22:22:44 +0530

    Commiting store,action creators and reducer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Listing from "./components/Listing";
+import Description from "./components/Description";
 
-export default App;
+import { addRecipe } from "./actions/actionCreators";
+
+const App = props => {
+    return (
+        <div id="wrapper">
+            <Listing recipes={props.recipes} />
+            <Description />
+        </div>
+    );
+};
+
+const mapStateToProps = state => {
+    return {
+        recipes: state.recipes
+    };
+};
+
+const mapDispatchToProps = dispatch => ({
+    addRecipe: value => dispatch(addRecipe(value))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
